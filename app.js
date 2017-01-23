ac8f5403615010901efd35c3ee7b4645babe5627
GuillotJessica <babaorhum001@gmail.com>
2017-01-23 22:06:16 +0100

    fin technic

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let moveDown = function() {
     $('h2').animate({
         top: '80vh'
-    }, 3000, 'swing');
+    }, 1000, 'swing');
     $('h1').animate({
         top: '88vh'
-    }, 3000, 'swing', function() {
+    }, 1000, 'swing', function() {
         $('nav').fadeIn(4000);
     })
 }
@@ -14,12 +14,31 @@ let moveDown = function() {
 
 
 $(document).ready(function() {
-    moveDown()
-    $( ".header" ).hover(function() {
-      // `this` is the DOM element that was clicked
-      var index = $( ".header" ).index( this );
-      $( ".snippet" ).removeClass('selected');
-      $( ".snippet" ).eq(index).addClass('selected');
+    if (window.matchMedia("(min-width: 800px)").matches) {
+        moveDown()
+        $(".header").hover(function() {
+            // `this` is the DOM element that was clicked
+            var index = $(".header").index(this);
+            $(".snippet").removeClass('selected');
+            $(".snippet").eq(index).addClass('selected');
 
-    });
+        });
+    } else {
+        $("h2").css('top', '0vh');
+        $("h1").css('top', '9vh');
+        $('h1').css('lineHeight', "1");
+        $(".contact").css('top', '2.6vh');
+console.log($('nav').height());
+$(".header").click(function() {
+    // `this` is the DOM element that was clicked
+    var index = $(".header").index(this);
+    $(".snippet").removeClass('selected');
+    $(".snippet").eq(index).addClass('selected');
+    $(".header").removeClass('borderbtt');
+    $(".header").eq(index).addClass('borderbtt');
+
+
+
+});
+    }
 });
